37f8d008d2a0d49d8474611717e365918b65a701
AhmedChabayta <chabays@gmail.com>
2022-04-30 23:47:56 +0300

    updated legend text

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import Element from "../components/Element";
+import Legend from "../components/Legend";
 import eData from "../elementsData.json";
 
 export default function Home() {
@@ -36,75 +37,9 @@ export default function Home() {
         {filteredElements.map((item, index) => (
           <div key={index} className="elements">
             <Element item={item} />
-            <div
-              key={1}
-              className="absolute flex space-x-5 font-thin"
-              style={{ gridColumn: 4, gridRow: 1 }}
-            >
-              <div className="flex flex-col space-y-2">
-                <input
-                  checked={checked}
-                  onChange={setChecked}
-                  type="radio"
-                  className="appearance-none checked:bg-[#32CD32] w-5 h-5"
-                />
-                <input
-                  checked={checked}
-                  onChange={setChecked}
-                  type="radio"
-                  className="appearance-none checked:bg-[#ffc8dd] w-5 h-5"
-                />
-                <input
-                  checked={checked}
-                  onChange={setChecked}
-                  type="radio"
-                  className="appearance-none checked:bg-[#800000] w-5 h-5"
-                />
-                <input
-                  checked={checked}
-                  onChange={setChecked}
-                  type="radio"
-                  className="appearance-none checked:bg-[#FFD700] w-5 h-5"
-                />
-                <input
-                  checked={checked}
-                  onChange={setChecked}
-                  type="radio"
-                  className="appearance-none checked:bg-[#FF7F50] w-5 h-5"
-                />
-              </div>
-              <div className="flex flex-col space-y-2 text-sm">
-                <div className="">Diatomic Nonmetal</div>
-                <div className="">Noble Gas</div>
-                <div className="">Alkali Metal</div>
-                <div className="">Alkaline Earth Metal</div>
-                <div className="">Metalloid</div>
-              </div>
-              <div className="flex flex-col space-y-2">
-                <div className="bg-[#adc178] w-5 h-5" />
-                <div className="bg-[#cdb4db] w-5 h-5" />
-                <div className="bg-[#00b4d8] w-5 h-5" />
-                <div className="bg-[#FFD700] w-5 h-5" />
-                <div className="bg-[#0077b6] w-5 h-5" />
-              </div>
-              <div className="flex flex-col space-y-2 text-sm">
-                <div className="">Polyatomic Nonmetal</div>
-                <div className="">Post-Transition Metal</div>
-                <div className="">Transition Metal</div>
-                <div className="">Alkaline Earth Metal</div>
-                <div className="">Lanthanide</div>
-              </div>
-              <div className="flex flex-col space-y-2">
-                <div className="bg-[#C8729D] w-5 h-5" />
-                <div className="bg-[#abc4ff] w-5 h-5" />
-              </div>
-              <div className="flex flex-col space-y-2 text-sm">
-                <div className="">Actinide</div>
-                <div className="">Unknown</div>
-              </div>
-            </div>
           </div>
         ))}
+        <Legend />
       </div>
     </div>
   );
